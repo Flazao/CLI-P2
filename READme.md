d243f8b21f6918dd24d7e22f371802847eac7550
Gabriel Flazao <gabrielflazaopoletti1502@gmail.com>
2025-05-22 21:27:22 -0300

    Adicionando o que eu acho que vai ter de função

--- READme.md
+++ READme.md
@@ -1,1 +1,5 @@
+Gabriel Flazao - 1990590
+Juliana Moreno - 1994729
+Maria Fernanda de Andrade - 1998066
+
 Sistema de Controle de biblioteca
---
+++ main.py
@@ -0,0 +1,22 @@
+print("=-" * 20)
+print("CLI de Controle de Biblioteca")
+print("=-" * 20)
+
+usuarios = {}
+livros = {}
+
+def cadastrar_usuario():
+    return None
+
+def cadastrar_livro():
+    return None
+
+def emprestar_livro():
+    return None
+
+def devolver_livro():
+    return None
+
+def ver_historico_usuario():
+    return None
+
