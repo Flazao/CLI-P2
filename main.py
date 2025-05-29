172066b94e121a290443a3596535ecee29f419c3
Gabriel Flazao <gabrielflazaopoletti1502@gmail.com>
2025-05-29 04:11:01 -0300

    Adicionando função exibir usuario/livro e melhorando visualização no terminal

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ print("CLI de Controle de Biblioteca")
 print("=-" * 20)
 
 usuarios = []
-emprestimos = []
 livros = []
+emprestimos = []
 
 contagem_de_cadastro = 0
 contagem_de_livro = 0
@@ -15,11 +15,11 @@ def cadastrar_usuario(nome: str, cpf: int):
     id_usuario = 1000 + contagem_de_cadastro
     contagem_de_cadastro += 1
     user = {
-        "id_usuario":id_usuario,
         "nome":nome,
+        "id_usuario":id_usuario,
         "cpf":cpf}
     usuarios.append(user)
-    print(f"Usuario cadastrado com sucesso!\n O usuario {nome}, recebeu o id: {id_usuario}.")
+    print(f"Usuario cadastrado com sucesso!\n O usuario {nome}, recebeu o id: {id_usuario}.\n")
 
 def validar_cpf(cpf):
     contar_cpf = str(cpf)
@@ -40,7 +40,7 @@ def cadastrar_livro(nome, genero, autor):
         "disponivel": True
     }
     livros.append(livro)
-    return print(f"livro cadastrado com sucesso!\n O livro {nome} recebeu o id {id_livro}.")
+    return print(f"livro cadastrado com sucesso!\n O livro {nome} recebeu o id {id_livro}.\n")
 
 def emprestar_livro(usuarios, livros, emprestimos, id_usuario, id_livro):
     return None
@@ -51,23 +51,49 @@ def devolver_livro():
 def ver_historico_usuario():
     return None
 
+def exibir_usuarios():
+    if usuarios:
+        for usuario in usuarios:
+            for chave, valor in usuario.items():
+                print(f'{chave}: {valor}')
+            print(' ')
+    else:
+        print("Nenhum usuário foi cadastrado")
+
+
+def exibir_livros():
+    if livros:
+        for livro in livros:
+            for chave, valor in livro.items():
+                print(f'{chave}: {valor}')
+            print(' ')
+    else:
+        print('Nenhum livro foi cadastrado')
+
+
 print("\nOlá seja bem-vindo!")
 print("Qual operação deseja realizar?")
-print("1 para cadastrar usuario")
-print("2 para cadastrar livro")
-print("3 para emprestimo de livro")
-print("4 para devolver livro")
-print("5 para ver o Historico de Emprestimo")
-print("6 para deletar livro")
-print("7 para deletar usuario")
-print("0 para finalizar\n")
+print("""
+1 para cadastrar usuario
+2 para cadastrar livro
+3 para ver usuarios cadastrados
+4 para ver livros cadastrados
+5 para emprestimo de livro
+6 para devolver livro
+7 para ver o Historico de Emprestimo
+8 para deletar usuario
+9 para deletar livro
+0 para finalizar
+      """)
 
 while True:
-    operacao = int(input("Insira a opção: "))
+    operacao = int(input("\033[32mInsira a opção: \033[m"))
+    print(' ')
 
     if operacao == 0:
         break
-    if operacao == 1:
+    
+    elif operacao == 1:
         nome = str(input("Nome do usuário: "))
         cpf = int(input("CPF do usuário: "))
         if validar_cpf(cpf) == False:
@@ -82,8 +108,24 @@ while True:
         cadastrar_livro(nome, genero, autor)
 
     elif operacao == 3:
+        exibir_usuarios()
+
+    elif operacao == 4:
+        exibir_livros()
+
+    elif operacao == 5:
         None
 
+    elif operacao == 6:
+        None
+
+    elif operacao == 7:
+        None    
+
+    elif operacao == 8:
+        None
+
+    
 #testes 
     elif operacao == 10:
         user_id = str(input("user "))
@@ -91,7 +133,3 @@ while True:
         emprestar_livro(usuarios, livros, emprestimos, livro_id, user_id)
     else:
         print("Opção inválida, tente novamente")
-
-
-print(usuarios)
-print(livros)
